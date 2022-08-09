fbf57cc8d77f28d5ebad56a87bae37a175848989
anushkaSolanki <110100940+anushkaSolanki@users.noreply.github.com>
2022-08-09 16:52:36 +0530

    Delete springboot directory

--- springboot/src/main/java/com/springmongo/springboot/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springmongo.springboot;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookRepository extends MongoRepository<Books,Integer>{
-    
-}
